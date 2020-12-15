7c338f77942c774575560a7d8d5fe3995072d45c
Cody <55256070+codyhandy@users.noreply.github.com>
2020-12-15 15:31:17 -0700

    uploaded assignment 5

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: './js/src/main.js',
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, 'build')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(m?js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [{
+                    loader: MiniCSSExtractPlugin.loader
+                }, 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            filename: './index.html',
+            favicon: './favicon.ico'
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new MiniCSSExtractPlugin({
+            filename: 'main.css'
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'build'),
+        compress: true,
+        port: 9001
+    }
+}
